e02d8e7dfeb110bad2697d0cb60845b631ddb878
alexander-ayer <157054492+alexander-ayer@users.noreply.github.com>
2025-03-10 20:36:52 -0400

    Added name to App

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App(): React.JSX.Element {
                 UM COS420 with React Hooks and TypeScript
             </header>
             <p>
-                Edit <code>src/App.tsx</code> and save. This page will
+                <code>Alex</code> and save. This page will
                 automatically reload.
             </p>
         </div>
